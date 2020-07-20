757831d689d25929d2abe544971273b936009c38
Micah Kirkpatrick <micahkirkpatrick@yahoo.com>
2020-07-20 14:30:04 -0400

    Add files via upload

---
+++ variables/variables6.rs
@@ -0,0 +1,8 @@
+// variables6.rs
+// Make me compile! Execute the command `rustlings hint variables6` if you want a hint :)
+
+
+const NUMBER: u32 = 300_000_000;
+fn main() {
+    println!("Number {}", NUMBER);
+}
